03842d0aa753a73f23fc36b036b336f595a7e303
YongHo <skymo6872@gmail.com>
2025-12-22 09:48:33 +0900

    코드카다 1번문제 제출

--- CodeKata1/main.cpp
+++ CodeKata1/main.cpp
@@ -3,34 +3,34 @@
 using namespace std;
 
 
-void Substract(int num1, int num2) {
-	cout << num1 << " - " << num2 << " = " << num1 - num2 << endl;
+int solution(int num1, int num2) {
+	return num1 - num2;
 }
 
 
 
 int main() {
 	int num1, num2;
-		cout << "두 정수를 입력해주세요." << endl;
-		cin >> num1 >> num2;
-		while (1) {
-			if (num1 < -50000 || num2 < -50000) {
-				cout << "너무 작은 정수를 입력하였습니다. -50000 이상의 정수를 입력해주세요." << endl;
+	cout << "두 정수를 입력해주세요." << endl;
+	cin >> num1 >> num2;
+	while (1) {
+		if (num1 < -50000 || num2 < -50000) {
+			cout << "너무 작은 정수를 입력하였습니다. -50000 이상의 정수를 입력해주세요." << endl;
+			cin >> num1 >> num2;
+		}
+		else {
+			if (num1 > 50000 || num2 > 50000) {
+				cout << "너무 큰 정수를 입력하였습니다. 50000 이하의 정수를 입력해주세요." << endl;
 				cin >> num1 >> num2;
 			}
 			else {
-				if (num1 > 50000 || num2 > 50000) {
-					cout << "너무 큰 정수를 입력하였습니다. 50000 이하의 정수를 입력해주세요." << endl;
-					cin >> num1 >> num2;
-				}
-				else {
-					break;
-				}
+				break;
 			}
-			
 		}
 
-	Substract(num1, num2);
+	}
+
+	cout << num1 << " - " << num2 << " = " << solution(num1, num2) << endl;
 
 	return 0;
 }
