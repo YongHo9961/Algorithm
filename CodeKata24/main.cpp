8da44be6f00a19c8222e899d1364daff86e910e7
YongHo <skymo6872@gmail.com>
2026-01-22 09:15:41 +0900

    CodeKata 24번 문제 제출

---
+++ CodeKata24/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<string> seoul)
+{
+	string answer = "";
+	for (size_t i = 0; i < seoul.size(); i++)
+	{
+		if (seoul[i] == "Kim")
+		{
+			answer = "김서방은 " + to_string(i) + "에 있다";
+			
+		}
+	}
+	return answer;
+}
+
+int main() {
+	vector<string> temp = { "Jane", "Kim" };
+
+	cout << solution(temp) << endl;
+
+
+	return 0;
+}
