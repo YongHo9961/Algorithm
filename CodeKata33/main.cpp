aa314b7919a725b528750ff4ed0082d96a47ac01
YongHo <skymo6872@gmail.com>
2026-02-04 09:25:46 +0900

    CodeKata 33번 문제 제출

---
+++ CodeKata33/main.cpp
@@ -0,0 +1,31 @@
+
+
+using namespace std;
+
+int solution(int left, int right)
+{
+	int answer = 0;
+	for (int i = left; i <= right; i++)
+	{
+		int DivisorCount = 0;
+
+		for (int j = 1; j <= i; j++)
+		{
+			if (i % j == 0)
+			{
+				DivisorCount++;
+			}
+		}
+
+		if (DivisorCount % 2 == 0)
+		{
+			answer += i;
+		}
+		else
+		{
+			answer -= i;
+		}
+	}
+
+	return answer;
+}
