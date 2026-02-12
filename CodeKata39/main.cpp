c34d482522b058bcd30b8bd6e9abca3ded5ec240
YongHo <skymo6872@gmail.com>
2026-02-12 09:40:19 +0900

    CodeKata 39번 문제 제출

---
+++ CodeKata39/main.cpp
@@ -0,0 +1,24 @@
+﻿#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(int a, int b)
+{
+  vector<int> answer;
+  int gcd;
+  
+  for (int i = min(a,b); i > 0; i--)
+  {
+    if (a % i == 0 && b % i == 0)
+    {
+      answer.push_back(i);
+      gcd = i;
+      break;
+    }
+  }
+  long long lcm = (a / gcd) * b;
+  answer.push_back(lcm);
+
+  return answer;
+}
