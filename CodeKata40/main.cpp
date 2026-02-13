a1861ab0847730ae4cd8911d211a3db8a62a77ba
YongHo <skymo6872@gmail.com>
2026-02-13 09:33:46 +0900

    CodeKata 40번 문제 제출

---
+++ CodeKata40/main.cpp
@@ -0,0 +1,24 @@
+﻿#include <vector>
+
+using namespace std;
+
+int solution(int n)
+{
+  vector<int> temp;
+  int answer = 0;
+  int multiplier = 1;
+  while (n > 0)
+  {
+    temp.push_back(n % 3);
+    n /= 3;
+
+  }
+  // 0 0 2 1
+  for (int i = temp.size() - 1; i >= 0; i--)
+  {
+    answer += temp[i] * multiplier;
+    multiplier *= 3;
+  }
+
+  return answer;
+}
