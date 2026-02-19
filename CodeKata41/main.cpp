7e9c5a6197d17dcc8cf8ea310bc1fd6f79b47b09
YongHo <skymo6872@gmail.com>
2026-02-19 09:08:15 +0900

    CodeKata 41번 문제 제출

---
+++ CodeKata41/main.cpp
@@ -0,0 +1,31 @@
+﻿#include <string>
+#include <cctype>
+
+using namespace std;
+
+string solution(string s)
+{
+  int IndexNum = 0;
+
+  for (int i = 0; i < s.size(); i++)
+  {
+    if (s[i] == ' ')
+    {
+      IndexNum = 0;
+    }
+    else
+    {
+      if (IndexNum % 2 == 0)
+      {
+        s[i] = toupper(s[i]);
+      }
+      else
+      {
+        s[i] = tolower(s[i]);
+      }
+      IndexNum++;
+    }
+  }
+
+  return s;
+}
