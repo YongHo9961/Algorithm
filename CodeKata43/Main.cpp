1de1ea518396ee4edb6814e95431c6e73bbc0e7f
YongHo <skymo6872@gmail.com>
2026-02-23 09:18:19 +0900

    CodeKata 43번 문제 제출

---
+++ CodeKata43/Main.cpp
@@ -0,0 +1,24 @@
+﻿#include <string>
+
+using namespace std;
+
+int solution(string t, string p)
+{
+  int answer = 0;
+
+  for (int i = 0; i < t.size() - (p.size() - 1); i++)
+  {
+    string temp;
+    for (int j = 0; j < p.size(); j++)
+    {
+      temp += t[i + j];
+    }
+
+    if (temp <= p)
+    {
+      answer++;
+    }
+  }
+
+  return answer;
+}
