e27f8087a6d3d44ea24300035413a76465d868b6
YongHo <skymo6872@gmail.com>
2026-02-24 09:29:34 +0900

    CodeKata 44번 문제 제출

---
+++ CodeKata44/main.cpp
@@ -0,0 +1,31 @@
+﻿#include <vector>
+
+using namespace std;
+
+int solution(vector<vector<int>> sizes)
+{
+  int answerindex1 = 0;
+  int answerindex2 = 0;
+  for (size_t i = 0; i < sizes.size(); i++)
+  {
+    if (sizes[i][0] < sizes[i][1])
+    {
+      int temp = sizes[i][0];
+      sizes[i][0] = sizes[i][1];
+      sizes[i][1] = temp;
+    }
+
+    if (answerindex1 < sizes[i][0])
+    {
+      answerindex1 = sizes[i][0];
+    }
+
+    if (answerindex2 < sizes[i][1])
+    {
+      answerindex2 = sizes[i][1];
+    }
+   
+  }
+
+  return answerindex1 * answerindex2;
+}
