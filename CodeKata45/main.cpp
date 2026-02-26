4140bb2673c169d8fc2138ac8adc63c36dd36c64
YongHo <skymo6872@gmail.com>
2026-02-26 09:38:12 +0900

    CodeKata 45번 문제 제출

---
+++ CodeKata45/main.cpp
@@ -0,0 +1,35 @@
+﻿#include <string>
+
+using namespace std;
+
+string solution(string s, int n)
+{
+  for (size_t i = 0; i < s.size(); i++)
+  {
+    if (s[i] == ' ')
+    {
+
+    }
+    else
+    {
+      if (s[i] < 'a')
+      {
+        char temp = s[i] - 'A';
+        temp += n;
+        temp = temp % 26;
+        temp += 'A';
+        s[i] = temp;
+      }
+      else
+      {
+        char temp = s[i] - 'a';
+        temp += n;
+        temp = temp % 26;
+        temp += 'a';
+        s[i] = temp;
+      }
+    }
+  }
+
+  return s;
+}
